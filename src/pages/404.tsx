f8d81ff82a874a136be363e389164c6cda61efbb
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-01 11:32:35 +0900

    replace router with link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,16 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import * as CS from 'src/common/components/Styles';
-import * as S from 'src/common/components/MainView/Styles';
 
 function Custom404() {
-  const router = useRouter();
-
   return (
     <>
-      <S.MainImg
-        src="/images/studio/SYD_0140.jpg"
-        alt="Page Not Found"
-        onClick={() => router.push('/')}
-      />
+      <Link href="/">
+        <CS.MainImg
+          src="/images/studio/SYD_0140.jpg"
+          alt="Page Not Found"
+        />
+      </Link>
       <CS.MainInfo>&nbsp;</CS.MainInfo>
     </>
   );
