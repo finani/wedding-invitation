25fc5574ffad324cee75be97527ba12bf1ca314e
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-04 23:48:02 +0900

    add Contact

--- src/common/components/Contact/Styles.ts
+++ src/common/components/Contact/Styles.ts
@@ -1,5 +1,79 @@
+import { media } from 'src/common/utils/styles';
 import styled from 'styled-components';
 
-export const MainImg = styled.img`
-  width: 100%;
+export const ContactContainer = styled.div`
+  padding: 20px 0px;
+`;
+
+export const ContactTitle = styled.h1`
+  margin: 1em;
+
+  font-size: 4vw;
+  ${media.medium} {
+    font-size: 5vw;
+  }
+  ${media.xSmall} {
+    font-size: 6vw;
+  }
+  text-align: center;
+`;
+
+export const ContactTable = styled.table`
+  margin: auto;
+
+  font-size: 2vw;
+  ${media.medium} {
+    font-size: 3vw;
+  }
+  ${media.xSmall} {
+    font-size: 4vw;
+  }
+  text-align: center;
+
+  line-height: 2em;
+  letter-spacing: 6px;
+`;
+
+export const ContactGroomCircle = styled.div`
+  width: 45px;
+  height: 45px;
+  background-color: #70b5f9;
+  border-radius: 50%;
+  margin: 0.5em;
+
+  position: relative;
+  display: inline-block;
+`;
+
+export const ContactBrideCircle = styled.div`
+  width: 45px;
+  height: 45px;
+  background-color: #e7a33e;
+  border-radius: 50%;
+  margin: 0.5em;
+
+  position: relative;
+  display: inline-block;
+`;
+
+export const ContactIcon = styled.div`
+  position: absolute;
+  left: 22.5%;
+  top: 15%;
+`;
+
+export const ContactText = styled.h4`
+  margin: 2em;
+
+  font-size: 1vw;
+  ${media.medium} {
+    font-size: 1.5vw;
+  }
+  ${media.xSmall} {
+    font-size: 2vw;
+  }
+  text-align: left;
+
+  line-height: 1.5em;
+  letter-spacing: 1px;
 `;
--- src/common/components/Contact/index.tsx
+++ src/common/components/Contact/index.tsx
@@ -1,20 +1,122 @@
+import PhoneIcon from '@mui/icons-material/Phone';
+import SmsIcon from '@mui/icons-material/Sms';
+
 import * as CS from 'src/common/components/Styles';
 import * as S from './Styles';
 
 function Contact() {
   return (
-    <CS.EvenContainer>
-      <CS.MainInfoGridList>
-        <CS.MainDetails>
-          <CS.Title>
-            <br></br>
-            Contact
-            <br></br>
-          </CS.Title>
-        </CS.MainDetails>
-        <CS.MainImg src="/images/mobile/SYD_0056.jpg" alt="Love, love, love" />
-      </CS.MainInfoGridList>
-    </CS.EvenContainer>
+    <S.ContactContainer>
+      <CS.MainInfo>
+        <S.ContactTable>
+          <tbody>
+            <tr>
+              <td>신랑에게 연락하기</td>
+              <td>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+              </td>
+              <td>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+              </td>
+            </tr>
+            <tr>
+              <td>신부에게 연락하기</td>
+              <td>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+              </td>
+              <td>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+              </td>
+            </tr>
+          </tbody>
+        </S.ContactTable>
+        <S.ContactTitle>혼주에게 연락하기</S.ContactTitle>
+        <S.ContactTable>
+          <tbody>
+            <tr>
+              <td>신랑측 혼주</td>
+              <td>신부측 혼주</td>
+            </tr>
+            <tr>
+              <td>아버지 위희량</td>
+              <td>아버지 김태우</td>
+            </tr>
+            <tr>
+              <td>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+              </td>
+              <td>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+              </td>
+            </tr>
+            <tr>
+              <td>어머니 조단자</td>
+              <td>어머니 박소연</td>
+            </tr>
+            <tr>
+              <td>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+                <S.ContactGroomCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactGroomCircle>
+              </td>
+              <td>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <PhoneIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+                <S.ContactBrideCircle>
+                  <S.ContactIcon>
+                    <SmsIcon fontSize="medium" style={{ color: 'white' }} />
+                  </S.ContactIcon>
+                </S.ContactBrideCircle>
+              </td>
+            </tr>
+          </tbody>
+        </S.ContactTable>
+      </CS.MainInfo>
+    </S.ContactContainer>
   );
 }
 
