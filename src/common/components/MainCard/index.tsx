5be4c6cc879647ee6dcff0b26b9e034f95c0e67a
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-08 00:00:59 +0900

    add temp mainCard

--- src/common/components/MainCard/index.tsx
+++ src/common/components/MainCard/index.tsx
@@ -5,15 +5,7 @@ import * as CS from 'src/common/components/Styles';
 function MainCard() {
   return (
     <CS.CommonContainer>
-      <CS.CommonTitle>
-        인환, 채윤
-        <br></br>
-        <br></br>
-        <Link href='/'>우리 결혼!</Link>
-      </CS.CommonTitle>
-      <CS.CommonSubTitle>
-        <CS.MainImg src='/images/mobile/SYD_0602.jpg' alt='Love, love, love' />
-      </CS.CommonSubTitle>
+      <CS.MainImg src='/images/mainCard.png' alt='Love, love, love' />
     </CS.CommonContainer>
   );
 }
