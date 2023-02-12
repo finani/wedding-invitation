42913522de5ada6756abe6f2b05351908a30fcad
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-12 23:10:41 +0900

    edit font size (invitation)

--- src/common/components/Invitation/Styles.ts
+++ src/common/components/Invitation/Styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InvitationBody = styled.h4`
   margin: 1rem;
 
-  font-size: 0.8rem;
+  font-size: 0.9rem;
   text-align: center;
 
   line-height: 1.5rem;
