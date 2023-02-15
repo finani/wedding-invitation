410ac9759bddb063534e952e0cc0585615285e03
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-15 23:18:41 +0900

    add new main card

--- src/common/components/MainCard/index.tsx
+++ src/common/components/MainCard/index.tsx
@@ -3,7 +3,7 @@ import * as CS from 'src/common/components/Styles';
 function MainCard() {
   return (
     <CS.CommonContainer>
-      <CS.MainImg src="/images/mainCard2_resize.png" alt="Love, love, love" />
+      <CS.MainImg src="/images/main_card3.jpg" alt="Love, love, love" />
       <CS.CommonMargin margin="40px" />
     </CS.CommonContainer>
   );
