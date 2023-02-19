ac8fd8f9dbf81beb0006eda6ed746ef19ace9f5b
Inhwan Wee <10ezenfinani@gmail.com>
2023-02-19 12:24:35 +0900

    fix flowerCard link

--- src/common/components/FlowerCard/index.tsx
+++ src/common/components/FlowerCard/index.tsx
@@ -11,31 +11,31 @@ function FlowerCard() {
           alt="Love, love, love"
         />
         <S.FlowerCardBody>
-          <S.FlowerCardTextContainer>
-            <CS.CommonTable>
-              <tbody>
-                <tr>
-                  <S.FlowerCardTd>
-                    <S.FlowerCardP>신랑, 신부님께</S.FlowerCardP>
-                    <S.FlowerCardP>축하의 마음을 전해보세요</S.FlowerCardP>
-                    <S.FlowerCardPBold>
-                      <b>화환 선물하기 ►</b>
-                    </S.FlowerCardPBold>
-                  </S.FlowerCardTd>
-                  <td>
-                    <a href="https://www.barunsonflower.com/?barunid=301069">
+          <a href="https://www.barunsonflower.com/?barunid=301069">
+            <S.FlowerCardTextContainer>
+              <CS.CommonTable>
+                <tbody>
+                  <tr>
+                    <S.FlowerCardTd>
+                      <S.FlowerCardP>신랑, 신부님께</S.FlowerCardP>
+                      <S.FlowerCardP>축하의 마음을 전해보세요</S.FlowerCardP>
+                      <S.FlowerCardPBold>
+                        <b>화환 선물하기 ►</b>
+                      </S.FlowerCardPBold>
+                    </S.FlowerCardTd>
+                    <td>
                       <S.FlowerCardImageContainer>
                         <CS.MainImg
                           src="/images/barunson/flowers_img_v2.png"
                           alt="Love, love, love"
                         />
                       </S.FlowerCardImageContainer>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </CS.CommonTable>
-          </S.FlowerCardTextContainer>
+                    </td>
+                  </tr>
+                </tbody>
+              </CS.CommonTable>
+            </S.FlowerCardTextContainer>
+          </a>
         </S.FlowerCardBody>
       </CS.CommonBody>
       <CS.CommonMargin margin="50px" />
